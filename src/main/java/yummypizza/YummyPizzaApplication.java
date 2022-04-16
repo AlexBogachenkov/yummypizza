256e3a5a298903a7520ff903d2f9f62e37c64fe1
AlexBogachenkov <aleksbogac@gmail.com>
2022-04-16 22:56:10 +0300

    Initial commit

---
+++ src/main/java/yummypizza/YummyPizzaApplication.java
@@ -0,0 +1,14 @@
+package yummypizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+public class YummyPizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YummyPizzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yummypizza/YummyPizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package yummypizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YummyPizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
